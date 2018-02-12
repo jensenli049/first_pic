5827a35273e2c505a9e7e12cce1610fe21ea8674
Jensen Li <jensenli049@gmail.com>
2018-02-12 09:37:45 -0500

    fixed -> ppm should work

--- first_pic.c
+++ first_pic.c
@@ -9,15 +9,18 @@
 int main(){
   int fd = open("first.ppm", O_CREAT | O_WRONLY, 0644);
   char buffer[512];
-  char * header = "P6\n512 512\n255\n";
+  char * header = "P3\n512 512\n255\n";
   int i, j, k, l, a = 255, b = 0, c = 0;
   write(fd, header, strlen(header));
 
   for(i = 0; i < 256; i++)
-    for(l = 0; l < 2; l++)
+    for(l = 0; l < 2; l++){
       for(j = 0; j < 256; j++)
 	for(k=0; k<2; k++){
-	  sprintf(buffer,"%c%c%c", a,(b+i+j)%256,c+j);
+	  sprintf(buffer,"%d %d %d ", a,(b+i+j)%256,c+j);
 	  write(fd, buffer, strlen(buffer));
 	}
+      sprintf(buffer,"\n");
+      write(fd, buffer, strlen(buffer));
+    }
 }
